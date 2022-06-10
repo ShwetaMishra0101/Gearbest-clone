cfc0e23e55615229d192f04b9dea41ce79925d65
ShwetaMishra0101 <93873372+ShwetaMishra0101@users.noreply.github.com>
2022-06-10 17:04:36 +0530

    navbar

---
+++ gearbest/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './navbar/Navbar';
+import Footer from './footer/Footer';
+
+function App() {
+  return (
+    <div className="App">
+     {/* <Navbar /> */}
+
+     <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ gearbest/src/footer/Footer.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import styled from './footer.module.css'
+const Footer = () => {
+    return <div style={{height:"800px",width:"100%",border:'1px solid blue'}}>
+
+        <div className={styled.footer} >
+
+            <div>
+                <img src="https://css.gbtcdn.com/imagecache/gbw/img/site/new-logo.png" alt="" />
+                <h4>AFFORDABLE QUALITY</h4>
+                <h4>FUN SHOOPING</h4>
+            </div>
+            <div>sd</div>
+            <div>sd</div>
+            <div>de</div>
+            <div>er</div>
+
+
+
+        </div>
+    </div>;
+}
+
+
+
+export default Footer;
